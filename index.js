6f9bb688b63c5e29de2534dc019b0031d88280d1
yemi <o.ogunbadejo7@gmail.com>
2026-05-06 13:19:03 +0100

    solved lab change

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-result =part1.slice(0,part1.length -1) + part1[part1.length -1].toUpperCase() + part2.slice(0,part2.length -1) + part2[part2.length -1].toUpperCase();
+const result =part1.slice(0,part1.length -1) + part1[part1.length -1].toUpperCase() + part2.slice(0,part2.length -1) + part2[part2.length -1].toUpperCase();
 console.log(result);
 
 // Print the cameLtaiL-formatted string
